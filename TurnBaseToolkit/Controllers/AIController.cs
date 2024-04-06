fc20ec9933354bb714eafd14168dfcd355aa042e
Dean Aviv <deanaviv5@gmail.com>
2024-04-06 13:48:04 +0300

    implemented state logic in the ai controller

--- TurnBaseToolkit/Controllers/AIController.cs
+++ TurnBaseToolkit/Controllers/AIController.cs
@@ -4,15 +4,21 @@ namespace TurnBaseToolkit
 {
     public class AIController : MonoBehaviour, IPlayTurn
     {
+        private IControllerState _currentState;
+
         private ITurnManager _turnManager;
         public void Initialize(ITurnManager manager)
         {
             _turnManager = manager;
+            SetState(new BalancedState());
         }
+
+        private void SetState(IControllerState state) => _currentState = state;
 
         public void BeginTurn()
         {
-            // Placeholder for AI turn initialization
+            // Placeholder for AI turn behaviour using the state pattern 
+            _currentState.PerformStateAction();
         }
 
         public void EndTurn()
